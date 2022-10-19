63eacc1f32f2c0f625a4c42a7f74ca25be7728a1
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-10-18 23:46:56 -0300

    Criando o fluxo de funcionamento do código

--- lista36.js
+++ lista36.js
@@ -1,1 +1,45 @@
 // BORAAAA COMEÇAR !!!
+var Torre = /** @class */ (function () {
+    function Torre(nome, ataque, alcance, nivel, valor) {
+        this.Nome = "";
+        this.Ataque = 0;
+        this.Alcance = 0;
+        this.Nivel = 0;
+        this.Valor = 0;
+        this.Nome = nome;
+        this.Ataque = ataque;
+        this.Alcance = alcance;
+        this.Nivel = nivel;
+        this.Valor = valor;
+    }
+    return Torre;
+}());
+var Inimigo = /** @class */ (function () {
+    function Inimigo(nome, vida) {
+        this.Nome = "";
+        this.Vida = 0;
+        this.Nome = nome;
+        this.Vida = vida;
+    }
+    Inimigo.prototype.ReceberDano = function (ataque) {
+        return this.Vida = this.Vida - ataque;
+    };
+    return Inimigo;
+}());
+var torres = [];
+var tabuleiro = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,];
+torres.length = 10;
+tabuleiro.length = 10;
+AdicionarTorre(new Torre("Arco", 3, 2, 1, 10), 6);
+IniciarPartida(2);
+function AdicionarTorre(torre, posicao) {
+    torres[posicao] = torre;
+}
+function IniciarPartida(inimigosTotais) {
+    var vidas = 10;
+    var numeroInimigos = inimigosTotais;
+    var inimigosDerrotados = 0;
+    var fimDeJogo = false;
+    do {
+    } while (!fimDeJogo);
+}
--- lista36.ts
+++ lista36.ts
@@ -1,1 +1,68 @@
 // BORAAAA COMEÇAR !!!
+
+class Torre{
+    Nome: string = ""
+    Ataque: number = 0
+    Alcance:number = 0
+    Nivel: number = 0
+    Valor: number = 0
+    
+
+    constructor(nome: string, ataque: number, alcance: number, nivel: number, valor: number){
+        this.Nome = nome
+        this.Ataque = ataque
+        this.Alcance = alcance
+        this.Nivel = nivel
+        this.Valor = valor
+
+    }
+}
+
+class Inimigo{
+    Nome: string = ""
+    Vida: number = 0
+
+    constructor(nome: string, vida: number){
+        this.Nome = nome
+        this.Vida = vida
+
+    }
+
+    public ReceberDano(ataque: number){
+        return this.Vida = this.Vida - ataque
+    }
+
+}
+
+let torres: Torre[] = []
+let tabuleiro: (Inimigo | undefined)[] = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined,]
+
+torres.length = 10
+tabuleiro.length = 10
+
+AdicionarTorre(new Torre("Arco", 3, 2, 1, 10), 6)
+
+IniciarPartida(2)
+
+function AdicionarTorre(torre: Torre, posicao: number){
+    torres[posicao] = torre
+}
+
+function IniciarPartida(inimigosTotais: number){
+    let vidas: number = 10
+    let numeroInimigos = inimigosTotais
+    let inimigosDerrotados = 0
+    let fimDeJogo: boolean = false
+
+    do{
+
+        
+
+
+
+    }while(!fimDeJogo)
+}
+
+
+
+
